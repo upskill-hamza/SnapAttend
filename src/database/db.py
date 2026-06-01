3d2e16a98cae12913b60d9c082e624a060bff62f
Hamza.io <hamza.upskilling.1@gmail.com>
2026-06-01 16:36:47 +0530

    Face pipeline completed

--- src/database/db.py
+++ src/database/db.py
@@ -34,3 +34,7 @@ def teacher_login(username, password):
     return None
     
         
+        
+def get_all_students():
+    response = supabase.table("students").select("*").execute()
+    return response.data
