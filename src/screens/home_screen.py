b4a3a8fced8338f5a751dad48a7bb95bd798d2bc
Hamza.io <hamza.upskilling.1@gmail.com>
2026-06-07 22:59:07 +0530

    removed footer_home

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from src.components.header import header_home
 from src.ui.base_layout import style_base_layout, style_background_home, style_background_dashboard
-from src.components.footer import footer_home, footer_dashboard
+from src.components.footer import footer_dashboard
 def home_screen():
     
     
